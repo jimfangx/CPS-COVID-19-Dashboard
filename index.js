d204992b0956b710bf56819f0e3b5aa4a8b476de
AirFusion <jfang.cv.ca.us@gmail.com>
2021-01-17 10:59:14 -0800

    added crontab timer

--- index.js
+++ index.js
@@ -1,80 +1,83 @@
-const puppeteer = require('puppeteer');
-const config = require('./config.json');
-var writtenWeeks = require('./writtenWeeks.json');
-const fs = require('fs');
+const schedule = require('node-schedule')
+// 0 0 */1 * *   */10 * * * *
+schedule.scheduleJob('*/5 * * * *', async function () {
+    const puppeteer = require('puppeteer');
+    const config = require('./config.json');
+    var writtenWeeks = require('./writtenWeeks.json');
+    const fs = require('fs');
+    (async () => {
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        await page.goto(config.covidDocs);
+        await page.waitForTimeout(2000)
 
-(async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto(`https://docs.google.com/document/d/1JOSWem1nFJZRoVMWGDN8FYBcMftzHRi-5yZ283NTBpE/preview`);
-    await page.waitForTimeout(2000)
+        var writeArray = []
 
-    var writeArray = []
+        writeArray = await page.evaluate(() => {
+            var returnArray = []
 
-    writeArray = await page.evaluate(() => {
-        var returnArray = []
+            var week = document.querySelector("#h\\.q4ulfv7pjpw3 > div > div > span:nth-child(2) > span > span:nth-child(2)").innerText
+            week = week.substring(0, week.indexOf('-')).replace(/\u200c/g, "")
+            week = week.split(' ')[2] + " " + week.split(' ')[3]
+            returnArray.push(week)
 
-        var week = document.querySelector("#h\\.q4ulfv7pjpw3 > div > div > span:nth-child(2) > span > span:nth-child(2)").innerText
-        week = week.substring(0, week.indexOf('-')).replace(/\u200c/g, "")
-        week = week.split(' ')[2] + " " + week.split(' ')[3]
-        returnArray.push(week)
+            var stuNumTest = document.querySelector("#kix-appview > div.kix-appview-editor-container > div > div:nth-child(1) > div.kix-zoomdocumentplugin-outer > div > div > div > div:nth-child(2) > div:nth-child(1) > div.kix-page-content-wrapper > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div:nth-child(6) > div > table > tbody > tr:nth-child(2) > td:nth-child(2)").innerText.trim().replace(/\u200c/g, "").replace(/ /g, "")
+            returnArray.push(stuNumTest)
 
-        var stuNumTest = document.querySelector("#kix-appview > div.kix-appview-editor-container > div > div:nth-child(1) > div.kix-zoomdocumentplugin-outer > div > div > div > div:nth-child(2) > div:nth-child(1) > div.kix-page-content-wrapper > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div:nth-child(6) > div > table > tbody > tr:nth-child(2) > td:nth-child(2)").innerText.trim().replace(/\u200c/g, "").replace(/ /g, "")
-        returnArray.push(stuNumTest)
+            var stuNumPos = document.querySelector("#kix-appview > div.kix-appview-editor-container > div > div:nth-child(1) > div.kix-zoomdocumentplugin-outer > div > div > div > div:nth-child(2) > div:nth-child(1) > div.kix-page-content-wrapper > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div:nth-child(6) > div > table > tbody > tr:nth-child(3) > td:nth-child(2)").innerText.trim().replace(/\u200c/g, "").replace(/ /g, "")
+            returnArray.push(stuNumPos)
 
-        var stuNumPos = document.querySelector("#kix-appview > div.kix-appview-editor-container > div > div:nth-child(1) > div.kix-zoomdocumentplugin-outer > div > div > div > div:nth-child(2) > div:nth-child(1) > div.kix-page-content-wrapper > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div:nth-child(6) > div > table > tbody > tr:nth-child(3) > td:nth-child(2)").innerText.trim().replace(/\u200c/g, "").replace(/ /g, "")
-        returnArray.push(stuNumPos)
+            var stuPercentPos = document.querySelector("#kix-appview > div.kix-appview-editor-container > div > div:nth-child(1) > div.kix-zoomdocumentplugin-outer > div > div > div > div:nth-child(2) > div:nth-child(1) > div.kix-page-content-wrapper > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div:nth-child(6) > div > table > tbody > tr:nth-child(4) > td:nth-child(2)").innerText.trim().replace('%', "").replace(/\u200c/g, "").replace(/ /g, "")
+            returnArray.push(stuPercentPos)
 
-        var stuPercentPos = document.querySelector("#kix-appview > div.kix-appview-editor-container > div > div:nth-child(1) > div.kix-zoomdocumentplugin-outer > div > div > div > div:nth-child(2) > div:nth-child(1) > div.kix-page-content-wrapper > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div:nth-child(6) > div > table > tbody > tr:nth-child(4) > td:nth-child(2)").innerText.trim().replace('%', "").replace(/\u200c/g, "").replace(/ /g, "")
-        returnArray.push(stuPercentPos)
+            var staffNumTest = document.querySelector("#kix-appview > div.kix-appview-editor-container > div > div:nth-child(1) > div.kix-zoomdocumentplugin-outer > div > div > div > div:nth-child(2) > div:nth-child(1) > div.kix-page-content-wrapper > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div:nth-child(6) > div > table > tbody > tr:nth-child(2) > td:nth-child(3)").innerText.trim().replace(/\u200c/g, "").replace(/ /g, "")
+            returnArray.push(staffNumTest)
 
-        var staffNumTest = document.querySelector("#kix-appview > div.kix-appview-editor-container > div > div:nth-child(1) > div.kix-zoomdocumentplugin-outer > div > div > div > div:nth-child(2) > div:nth-child(1) > div.kix-page-content-wrapper > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div:nth-child(6) > div > table > tbody > tr:nth-child(2) > td:nth-child(3)").innerText.trim().replace(/\u200c/g, "").replace(/ /g, "")
-        returnArray.push(staffNumTest)
+            var staffNumPos = document.querySelector("#kix-appview > div.kix-appview-editor-container > div > div:nth-child(1) > div.kix-zoomdocumentplugin-outer > div > div > div > div:nth-child(2) > div:nth-child(1) > div.kix-page-content-wrapper > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div:nth-child(6) > div > table > tbody > tr:nth-child(3) > td:nth-child(3)").innerText.trim().replace(/\u200c/g, "").replace(/ /g, "")
+            returnArray.push(staffNumPos)
 
-        var staffNumPos = document.querySelector("#kix-appview > div.kix-appview-editor-container > div > div:nth-child(1) > div.kix-zoomdocumentplugin-outer > div > div > div > div:nth-child(2) > div:nth-child(1) > div.kix-page-content-wrapper > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div:nth-child(6) > div > table > tbody > tr:nth-child(3) > td:nth-child(3)").innerText.trim().replace(/\u200c/g, "").replace(/ /g, "")
-        returnArray.push(staffNumPos)
+            var staffPercentPos = document.querySelector("#kix-appview > div.kix-appview-editor-container > div > div:nth-child(1) > div.kix-zoomdocumentplugin-outer > div > div > div > div:nth-child(2) > div:nth-child(1) > div.kix-page-content-wrapper > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div:nth-child(6) > div > table > tbody > tr:nth-child(4) > td:nth-child(3)").innerText.trim().replace('%', "").replace(/\u200c/g, "").replace(/ /g, "")
+            returnArray.push(staffPercentPos)
 
-        var staffPercentPos = document.querySelector("#kix-appview > div.kix-appview-editor-container > div > div:nth-child(1) > div.kix-zoomdocumentplugin-outer > div > div > div > div:nth-child(2) > div:nth-child(1) > div.kix-page-content-wrapper > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div:nth-child(6) > div > table > tbody > tr:nth-child(4) > td:nth-child(3)").innerText.trim().replace('%', "").replace(/\u200c/g, "").replace(/ /g, "")
-        returnArray.push(staffPercentPos)
+            var totalNumTest = document.querySelector("#kix-appview > div.kix-appview-editor-container > div > div:nth-child(1) > div.kix-zoomdocumentplugin-outer > div > div > div > div:nth-child(2) > div:nth-child(1) > div.kix-page-content-wrapper > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div:nth-child(6) > div > table > tbody > tr:nth-child(2) > td:nth-child(4)").innerText.trim().replace(/\u200c/g, "").replace(/ /g, "")
+            returnArray.push(totalNumTest)
 
-        var totalNumTest = document.querySelector("#kix-appview > div.kix-appview-editor-container > div > div:nth-child(1) > div.kix-zoomdocumentplugin-outer > div > div > div > div:nth-child(2) > div:nth-child(1) > div.kix-page-content-wrapper > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div:nth-child(6) > div > table > tbody > tr:nth-child(2) > td:nth-child(4)").innerText.trim().replace(/\u200c/g, "").replace(/ /g, "")
-        returnArray.push(totalNumTest)
+            var totalNumPos = document.querySelector("#kix-appview > div.kix-appview-editor-container > div > div:nth-child(1) > div.kix-zoomdocumentplugin-outer > div > div > div > div:nth-child(2) > div:nth-child(1) > div.kix-page-content-wrapper > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div:nth-child(6) > div > table > tbody > tr:nth-child(3) > td:nth-child(4)").innerText.trim().replace(/\u200c/g, "").replace(/ /g, "")
+            returnArray.push(totalNumPos)
 
-        var totalNumPos = document.querySelector("#kix-appview > div.kix-appview-editor-container > div > div:nth-child(1) > div.kix-zoomdocumentplugin-outer > div > div > div > div:nth-child(2) > div:nth-child(1) > div.kix-page-content-wrapper > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div:nth-child(6) > div > table > tbody > tr:nth-child(3) > td:nth-child(4)").innerText.trim().replace(/\u200c/g, "").replace(/ /g, "")
-        returnArray.push(totalNumPos)
+            var totalPercentPos = document.querySelector("#kix-appview > div.kix-appview-editor-container > div > div:nth-child(1) > div.kix-zoomdocumentplugin-outer > div > div > div > div:nth-child(2) > div:nth-child(1) > div.kix-page-content-wrapper > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div:nth-child(6) > div > table > tbody > tr:nth-child(4) > td:nth-child(4)").innerText.trim().replace('%', "").replace(/\u200c/g, "").replace(/ /g, "")
+            returnArray.push(totalPercentPos)
 
-        var totalPercentPos = document.querySelector("#kix-appview > div.kix-appview-editor-container > div > div:nth-child(1) > div.kix-zoomdocumentplugin-outer > div > div > div > div:nth-child(2) > div:nth-child(1) > div.kix-page-content-wrapper > div:nth-child(1) > div > div > div:nth-child(1) > div > div > div:nth-child(6) > div > table > tbody > tr:nth-child(4) > td:nth-child(4)").innerText.trim().replace('%', "").replace(/\u200c/g, "").replace(/ /g, "")
-        returnArray.push(totalPercentPos)
+            return returnArray
+        })
+        console.log(writeArray)
+        await page.goto(config.link);
+        await page.waitForTimeout(5000)
 
-        return returnArray
-    })
-    console.log(writeArray)
-    await page.goto(config.link);
-    await page.waitForTimeout(5000)
-
-    if (writtenWeeks[writtenWeeks.length - 1] != writeArray[0]) {
-        for (i = 0; i < writtenWeeks.length + 2; i++) { // add 2 for the header & the blank row that needs to be entered
-            if (i + 1 != writtenWeeks.length + 2) { // not the last one
-                await page.keyboard.press('ArrowDown');
-                await page.waitForTimeout(100)
-            } else { // last one - exec write
-                writtenWeeks.push(writeArray[0])
-                fs.writeFile('writtenWeeks.json', JSON.stringify(writtenWeeks), function (err) {
-                    if (err) console.log(err)
-                })
-                for (j = 0; j < 10; j++) {
-                    await page.keyboard.type(writeArray[j])
-                    await page.waitForTimeout(500)
-                    await page.keyboard.press('Tab');
-                    await page.waitForTimeout(500)
+        if (writtenWeeks[writtenWeeks.length - 1] != writeArray[0]) {
+            for (i = 0; i < writtenWeeks.length + 2; i++) { // add 2 for the header & the blank row that needs to be entered
+                if (i + 1 != writtenWeeks.length + 2) { // not the last one
+                    await page.keyboard.press('ArrowDown');
+                    await page.waitForTimeout(100)
+                } else { // last one - exec write
+                    writtenWeeks.push(writeArray[0])
+                    fs.writeFile('writtenWeeks.json', JSON.stringify(writtenWeeks), function (err) {
+                        if (err) console.log(err)
+                    })
+                    for (j = 0; j < 10; j++) {
+                        await page.keyboard.type(writeArray[j])
+                        await page.waitForTimeout(500)
+                        await page.keyboard.press('Tab');
+                        await page.waitForTimeout(500)
+                    }
+                    break;
                 }
-                break;
             }
         }
-    }
 
 
-    console.log(writeArray)
-    await browser.close();
-})();
+        console.log(writeArray)
+        await browser.close();
+    })();
 
+});
