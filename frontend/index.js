4101989b558a2585a057764e95c7207497bdfde6
AirFusion <jfang.cv.ca.us@gmail.com>
2021-01-19 17:23:33 -0800

    mobile support

--- frontend/index.js
+++ frontend/index.js
@@ -1,15 +1,28 @@
 $(function () {
+    if (window.innerWidth <= 800 && window.innerHeight <= 900) { // mobile detection
+        $('#dashboard').attr('src', "https://public.tableau.com/views/CPSCOVID-19/TotalsMobile?:showVizHome=no&:embed=true")
+    }
     $('#dashboard').attr('height', window.innerHeight - 74.5) //52.3 = "current dashboard" height
 });
 
 $('select').on('change', function () {
     if (this.value === 'total') {
-        $('#dashboard').attr('src', "https://public.tableau.com/views/CPSCOVID-19/Totals?:showVizHome=no&:embed=true")
-        
+        if (window.innerWidth <= 800 && window.innerHeight <= 900) { // mobile detection
+            $('#dashboard').attr('src', "https://public.tableau.com/views/CPSCOVID-19/TotalsMobile?:showVizHome=no&:embed=true")
+        } else {
+            $('#dashboard').attr('src', "https://public.tableau.com/views/CPSCOVID-19/Totals?:showVizHome=no&:embed=true")
+        }
     } else if (this.value === 'staff') {
-        $('#dashboard').attr('src', "https://public.tableau.com/views/CPSCOVID-19/Staff?:showVizHome=no&:embed=true")
+        if (window.innerWidth <= 800 && window.innerHeight <= 900) { // mobile detection
+            $('#dashboard').attr('src', "https://public.tableau.com/views/CPSCOVID-19/StaffMobile?:showVizHome=no&:embed=true")
+        } else {
+            $('#dashboard').attr('src', "https://public.tableau.com/views/CPSCOVID-19/Staff?:showVizHome=no&:embed=true")
+        }
     } else if (this.value === 'student') {
-        $('#dashboard').attr('src', "https://public.tableau.com/views/CPSCOVID-19/Students?:showVizHome=no&:embed=true")
-
+        if (window.innerWidth <= 800 && window.innerHeight <= 900) { // mobile detection
+            $('#dashboard').attr('src', "https://public.tableau.com/views/CPSCOVID-19/StudentMobile?:showVizHome=no&:embed=true")
+        } else {
+            $('#dashboard').attr('src', "https://public.tableau.com/views/CPSCOVID-19/Students?:showVizHome=no&:embed=true")
+        }
     }
 })
