d75dc32d630fd20493e064d425d448d8c4b0f9b0
AirFusion45 <jfang.cv.ca.us@gmail.com>
2021-03-26 15:34:43 -0700

    attempt to fix the alameda vac module

--- indexTest.js
+++ indexTest.js
@@ -7,36 +7,50 @@ const superagent = require('superagent');
     const browser = await puppeteer.launch({ headless: false, defaultViewport: null });
     const page = await browser.newPage();
 
-    var firstday = new Date(`2021-03-08`)
-    firstday.setDate(firstday.getDate() + 1)
-    console.log(firstday.toDateString())
+    await page.waitForTimeout(3000)
+    // new segment for vaccine & b117 - updated DAILY
+    await page.goto(`https://app.powerbigov.us/view?r=eyJrIjoiMzZhZDA1NDMtYjU4OS00NWNiLTliYmUtZWNmYzhiNDJiY2FiIiwidCI6IjMyZmRmZjJjLWY4NmUtNGJhMy1hNDdkLTZhNDRhN2Y0NWE2NCJ9`)
+    await page.waitForTimeout(3000)
+    var alamedaResult = null
+    var cdcCAStateVacTotals = null
+    var b117Cases = null
+    var statsPlot = await page.$('#pvExplorationHost > div > div > exploration > div > explore-canvas-modern > div > div.canvasFlexBox > div > div.displayArea.disableAnimations.actualSizeAlignCenter.actualSizeAlignTop.actualSizeOrigin > div.visualContainerHost > visual-container-repeat > visual-container-modern:nth-child(2) > transform > div > div:nth-child(3) > div')
+    await statsPlot.click({
+        button: 'right',
+    })
+    await page.waitForTimeout(2000)
+    var showAsTtable = await page.$(`body > div.default-contextmenu.dropdownOverlay.overlay.verticalScrollbar.themeableElement.overlayActive > drop-down-list > ng-transclude > ng-repeat > drop-down-list-item > ng-transclude > ng-switch > div`)
+    await showAsTtable.click()
 
-    var alamedaCases = null;
-    for (i = 0; i <= 7; i++) {
-        await page.goto(`https://data.chhs.ca.gov/api/3/action/datastore_search?resource_id=046cdd2b-31e5-4d34-9ed3-b48cdbc4be7a&q={"area":"alameda","date":"${firstday.getFullYear()}-${("0" + (firstday.getMonth() + 1)).slice(-2)}-${("0" + firstday.getDate()).slice(-2)}"}`)
-        page.waitForTimeout(1000)
-        var tempAlamedaCases = await page.evaluate((firstday) => {
-            var firstdayInternal = new Date(firstday)
-            if (JSON.parse(document.querySelector("body > pre").innerText).result.records.length > 1) { // multiple dates, check against firstday
-                for (x = 0; x < JSON.parse(document.querySelector("body > pre").innerText).result.records.length; x++) {
-                    if (JSON.parse(document.querySelector("body > pre").innerText).result.records[x].date.includes(`${firstdayInternal.getFullYear()}-${("0" + (firstdayInternal.getMonth() + 1)).slice(-2)}-${("0" + firstdayInternal.getDate()).slice(-2)}`)) {
-                        var alamedaCases = null
-                        alamedaCases = JSON.parse(document.querySelector("body > pre").innerText).result.records[x].cases
-                        return alamedaCases
-                    }
-                }
-            } else {
-                var alamedaCases = null
-                alamedaCases = JSON.parse(document.querySelector("body > pre").innerText).result.records[0].cases
-                return alamedaCases
-            }
-        }, firstday)
-        console.log(`${firstday.getFullYear()}-${("0" + (firstday.getMonth() + 1)).slice(-2)}-${("0" + firstday.getDate()).slice(-2)}`)
-        console.log(tempAlamedaCases)
-        alamedaCases += parseInt(tempAlamedaCases)
-        firstday.setDate(firstday.getDate() - 1)
-        console.log(firstday.toDateString())
+    // await autoScroll(page)
+
+    for (i = 0; i < 100; i++) {
+        await page.click("#pvExplorationHost > div > div > exploration > div > explore-canvas-modern > div > div.canvasFlexBox > div > div.displayArea.disableAnimations.fitToScreen > div.visualContainerHost > visual-container-repeat > visual-container-modern:nth-child(2) > transform > div > div:nth-child(3) > div > detail-visual-modern > div > visual-modern > div > div > div.pivotTable > div:nth-child(4) > div:nth-child(2)")
     }
-    console.log(alamedaCases)
+    await page.waitForTimeout(3000)
+    alamedaResult = await page.evaluate(() => {
+        var returnArray = []
+        var returnResult = {
+            "day": null,
+            "firstDose": null,
+            "secondDose": null
+        }
 
+        var tableHolderDivChildNumber = document.querySelector("#pvExplorationHost > div > div > exploration > div > explore-canvas-modern > div > div.canvasFlexBox > div > div.displayArea.disableAnimations.fitToScreen > div.visualContainerHost > visual-container-repeat > visual-container-modern:nth-child(2) > transform > div > div:nth-child(3) > div > detail-visual-modern > div > visual-modern > div > div > div.pivotTable > div.innerContainer > div.bodyCells > div").children.length // number of sections in the master table holder. rn its 2, could get larger. find the latest
+        for (i= 3; i>0; i--) { // get prev 3 days
+            var numberOfEntries = parseInt(document.querySelector(`#pvExplorationHost > div > div > exploration > div > explore-canvas-modern > div > div.canvasFlexBox > div > div.displayArea.disableAnimations.fitToScreen > div.visualContainerHost > visual-container-repeat > visual-container-modern:nth-child(2) > transform > div > div:nth-child(3) > div > detail-visual-modern > div > visual-modern > div > div > div.pivotTable > div.innerContainer > div.bodyCells > div > div:nth-child(${tableHolderDivChildNumber}) > div:nth-child(2)`).children.length)-i
+            returnResult.firstDose = document.querySelector(`#pvExplorationHost > div > div > exploration > div > explore-canvas-modern > div > div.canvasFlexBox > div > div.displayArea.disableAnimations.fitToScreen > div.visualContainerHost > visual-container-repeat > visual-container-modern:nth-child(2) > transform > div > div:nth-child(3) > div > detail-visual-modern > div > visual-modern > div > div > div.pivotTable > div.innerContainer > div.bodyCells > div > div:nth-child(${tableHolderDivChildNumber}) > div:nth-child(1) > div:nth-child(${numberOfEntries})`).textContent
+            returnResult.secondDose = document.querySelector(`#pvExplorationHost > div > div > exploration > div > explore-canvas-modern > div > div.canvasFlexBox > div > div.displayArea.disableAnimations.fitToScreen > div.visualContainerHost > visual-container-repeat > visual-container-modern:nth-child(2) > transform > div > div:nth-child(3) > div > detail-visual-modern > div > visual-modern > div > div > div.pivotTable > div.innerContainer > div.bodyCells > div > div:nth-child(${tableHolderDivChildNumber}) > div:nth-child(2) > div:nth-child(${numberOfEntries})`).textContent
+    
+            var numberOfDates = parseInt(document.querySelector("#pvExplorationHost > div > div > exploration > div > explore-canvas-modern > div > div.canvasFlexBox > div > div.displayArea.disableAnimations.fitToScreen > div.visualContainerHost > visual-container-repeat > visual-container-modern:nth-child(2) > transform > div > div:nth-child(3) > div > detail-visual-modern > div > visual-modern > div > div > div.pivotTable > div.innerContainer > div.rowHeaders > div").children.length)-i
+            returnResult.day = document.querySelector(`#pvExplorationHost > div > div > exploration > div > explore-canvas-modern > div > div.canvasFlexBox > div > div.displayArea.disableAnimations.fitToScreen > div.visualContainerHost > visual-container-repeat > visual-container-modern:nth-child(2) > transform > div > div:nth-child(3) > div > detail-visual-modern > div > visual-modern > div > div > div.pivotTable > div.innerContainer > div.rowHeaders > div > div:nth-child(${numberOfDates}) > div`).textContent
+
+            returnArray.push(returnResult)
+        }
+
+        
+        return returnResult
+    })
+    console.log(alamedaResult)
+    
 })();
